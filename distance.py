020f2a1cf9e839199471ee7bdc4eee035d505acc
みそかつ <84119754+misokats@users.noreply.github.com>
2026-04-21 15:26:20 +0900

    Fix variable name 'distance' to 'distance_mi'

--- distance.py
+++ distance.py
@@ -4,13 +4,13 @@ has_bike = False
 has_car = False
 has_ride_share_app = False
 
-if not distance <= 0:
+if not distance_mi <= 0:
     print(False)
 elif distance_mi <= 1 and is_raining:
     print(True)
 elif 6 >= distance_mi > 1 and has_bike and not is_raining:
     print(True)
-elif distance_mi > 6 and (has_car or has_ride_share _app):
+elif distance_mi > 6 and (has_car or has_ride_share_app):
     print(True)
 else:
     print(False) 
